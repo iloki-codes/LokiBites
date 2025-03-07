0e221700901d656f14a2db7622580c0f147f3968
Lokesh <lokesh.loki.codes@gmail.com>
2025-03-08 04:10:13 +0530

    build folder added

---
+++ server/dist/data.js
@@ -0,0 +1,164 @@
+export const sample_foods = [{
+  id: '1',
+  name: 'Pizza',
+  cookTime: '10-20',
+  price: 199,
+  favorite: false,
+  outlet: ["domino's"],
+  stars: 4.52,
+  imageUrl: 'https://www.allrecipes.com/thmb/CuYcPblAO4R0nCwf5qVXy6KLKac=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/1130239-01328eb8a0f544fe83b46e0aafcbf8a2.jpg',
+  tags: ['FastFood', 'Pizza', 'Lunch']
+}, {
+  id: '2',
+  name: 'Meatball',
+  price: 200,
+  cookTime: '20-30',
+  favorite: true,
+  outlet: ['Faasos'],
+  stars: 5,
+  imageUrl: 'https://b.zmtcdn.com/data/dish_photos/44d/265f8e0f7c900c5925865f1b1ae3244d.jpeg',
+  tags: ['SlowFood', 'Lunch']
+}, {
+  id: '3',
+  name: 'Crispy Burger',
+  price: 50,
+  cookTime: '10-15',
+  favorite: false,
+  outlet: ['burger king'],
+  stars: 3.5,
+  imageUrl: 'https://b.zmtcdn.com/data/dish_photos/289/a877cd684984b65a174b3e12582d4289.jpeg',
+  tags: ['FastFood', 'Hamburger']
+}, {
+  id: '4',
+  name: 'Fried Chicken',
+  price: 200,
+  cookTime: '15-20',
+  favorite: true,
+  outlet: ['kfc'],
+  stars: 3,
+  imageUrl: 'https://b.zmtcdn.com/data/dish_photos/054/7c1c9c67d06e7d1b765096abc58f2054.jpeg?output-format=webp',
+  tags: ['FastFood', 'Fry']
+}, {
+  id: '5',
+  name: 'Chicken Soup',
+  price: 110,
+  cookTime: '40-50',
+  favorite: false,
+  outlet: ['ama cafe'],
+  stars: 3.5,
+  imageUrl: 'https://b.zmtcdn.com/data/dish_photos/d39/73aa3d31209f4fc9712f22e8df31bd39.jpeg?output-format=webp',
+  tags: ['SlowFood', 'Soup']
+}, {
+  id: '6',
+  name: 'Chowmein',
+  price: 90,
+  cookTime: '8-10',
+  favorite: false,
+  outlet: ['ching chong cafe'],
+  stars: 4.0,
+  imageUrl: 'https://b.zmtcdn.com/data/dish_photos/6d1/d2b0a212876faa9f08bc4ba75e3b86d1.jpeg',
+  tags: ['FastFood', 'Lunch']
+},
+//
+
+{
+  id: '7',
+  name: 'Fried Momo',
+  price: 90,
+  cookTime: '5-9',
+  favorite: false,
+  outlet: ['ching chong cafe'],
+  stars: 4.5,
+  imageUrl: 'https://b.zmtcdn.com/data/dish_photos/172/031b73b22fbf5c24c26a3840285b9172.png?output-format=webp',
+  tags: ['FastFood', 'Chinese']
+}, {
+  id: '8',
+  name: 'Chhole Bhature',
+  price: 120,
+  cookTime: '10-15',
+  favorite: false,
+  outlet: ['ching chong cafe'],
+  stars: 3.5,
+  imageUrl: 'https://b.zmtcdn.com/data/pictures/chains/1/491/aa50c08ea5ade50a6d1c2d7aca242a1c_o2_featured_v2.jpg?output-format=webp',
+  tags: ['Breakfast', 'Lunch']
+}, {
+  id: '9',
+  name: 'Veg Thali',
+  price: 190,
+  cookTime: '10-20',
+  favorite: false,
+  outlet: ['singh dhaba'],
+  stars: 4.0,
+  imageUrl: 'https://b.zmtcdn.com/data/pictures/1/1301/ba145998485d6bdc7966bccd94008c77.jpg',
+  tags: ['FastFood', 'Pizza', 'Lunch']
+}, {
+  id: '10',
+  name: 'Non-Veg Thali',
+  price: 290,
+  cookTime: '10-20',
+  favorite: false,
+  outlet: ['khan dhaba'],
+  stars: 4.0,
+  imageUrl: 'https://b.zmtcdn.com/data/dish_photos/478/2d6968587cf7bb8b14c7eec912a43478.jpg?output-format=webp',
+  tags: ['FastFood', 'Pizza', 'Lunch']
+}, {
+  id: '11',
+  name: 'Manga Boba Tea',
+  price: 90,
+  cookTime: '8-10',
+  favorite: false,
+  outlet: ['bhanu beverages'],
+  stars: 4.0,
+  imageUrl: 'https://b.zmtcdn.com/data/dish_photos/7f8/148fb338e79c3aa06ffac5a7ccaf97f8.jpeg?fit=around|130:130&crop=130:130;*,*',
+  tags: ['FastFood', 'Pizza', 'Lunch']
+}, {
+  id: '12',
+  name: 'Filter Coffee',
+  price: 90,
+  cookTime: '8-10',
+  favorite: false,
+  outlet: ['ching chong cafe'],
+  stars: 3.0,
+  imageUrl: 'https://b.zmtcdn.com/data/dish_photos/bb4/7565421b6f0efa302923f5af00050bb4.jpg',
+  tags: ['FastFood', 'Pizza', 'Lunch']
+}];
+export const sample_tags = [{
+  name: 'All',
+  count: 6
+}, {
+  name: 'FastFood',
+  count: 4
+}, {
+  name: 'Pizza',
+  count: 2
+}, {
+  name: 'Lunch',
+  count: 3
+}, {
+  name: 'SlowFood',
+  count: 2
+}, {
+  name: 'Burger',
+  count: 1
+}, {
+  name: 'Fry',
+  count: 1
+}, {
+  name: 'Soup',
+  count: 1
+}];
+export const sample_users = [{
+  id: 1,
+  name: 'John Doe',
+  email: 'john@gmail.com',
+  password: '12345',
+  address: 'Toronto On',
+  isAdmin: false
+}, {
+  id: 2,
+  name: 'Jane Doe',
+  email: 'jane@gmail.com',
+  password: '12345',
+  address: 'Shanghai',
+  isAdmin: true
+}];
